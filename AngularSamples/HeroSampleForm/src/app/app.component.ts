b40eaaea5e530715a8c8033f2f28c926a67385df
John Moss <john.moss@qa.com>
2019-11-12 14:42:05 +0000

    Add of angular hero form sample

---
+++ AngularSamples/HeroSampleForm/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-hero-form></app-hero-form> 
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'HeroSampleForm';
+}
--- NodeJsExamples/StringPadder/string-padder.js
+++ NodeJsExamples/StringPadder/string-padder.js
@@ -20,24 +20,6 @@ StringPadder.prototype.pad = function(toPad, width) {
 	return output;
 }
 
-/**
-function padString(toPad, width) {
-
-	let i = 0;
-	let output = toPad;
-	while (i < width) {
-
-		output = "0" + output;
-		console.log(output);
-
-		i++;
-	}
-	return output;
-}
-
-let test = padString("22", 6);
-**/
-
 module.exports = StringPadder;
 
 
