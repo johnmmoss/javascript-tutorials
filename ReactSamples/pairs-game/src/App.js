17bff2a6f6650349edecbaa011e38b0645121314
John <johnmmoss@hotmail.co.uk>
2020-01-12 21:03:36 +0000

    Refactored pairs game so that pairs of numbers can be selected

--- ReactSamples/pairs-game/src/App.js
+++ ReactSamples/pairs-game/src/App.js
@@ -1,15 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import _ from 'underscore'
 
 function App() {
 
-  const [availableNumbers, setAvailableNumbers] = useState(_.range(1, 9));
-  const [gameNumbers, setGameNumbers] = useState(_.shuffle(availableNumbers.concat(availableNumbers)));
-  const shuffleNumbers = () => {
-    console.log("Shuffling numbers");
-    setGameNumbers(_.shuffle(availableNumbers.concat(availableNumbers)));
-    console.log(gameNumbers);
+  const generateGameNumbers = () => {
+    return _.shuffle((_.range(1, 9).concat(_.range(1, 9))));
+  }
+  const [gameNumbers, setGameNumbers] = useState(generateGameNumbers);
+  const [matchedNumbers, setMatchedNumbers] = useState([]);
+  const [candidateNumber1Index, setCandidateNumber1Index] = useState(-1);
+  const [candidateNumber2Index, setCandidateNumber2Index] = useState(-1);
+
+  const numberStatus = (index, number) => {
+    
+    if (matchedNumbers.includes(number)) {
+      return 'matched';
+    } else if (candidateNumber1Index === index ||
+               candidateNumber2Index === index) {
+      return 'candidate';
+    }
+    return 'hidden';
+  }
+
+  const manageGame = (index, number) => {
+
+    if (candidateNumber1Index < 0) {
+      setCandidateNumber1Index(index);
+      console.log("New Candidate 1");
+      return;
+    } 
+    
+    if (candidateNumber2Index < 0) {
+      setCandidateNumber2Index(index);
+      console.log("New Candidate 2");
+      return;
+    } 
+
+    // Otherwise, we have selected two numbers!
+    if (gameNumbers[candidateNumber1Index] === gameNumbers[candidateNumber2Index])  {
+
+      // we have a match
+      setMatchedNumbers([...matchedNumbers, gameNumbers[candidateNumber2Index]]);
+      setCandidateNumber1Index(index);
+      setCandidateNumber2Index(-1);
+      console.log("MATCH");
+      console.log("Next click index:" + index);
+    } else {
+      setCandidateNumber1Index(index);
+      setCandidateNumber2Index(-1);
+      console.log("No Match - Reset");
+    }
+
+    // When only two remaining we reduce.
   }
 
   return (
@@ -23,12 +66,21 @@ function App() {
           <div className="main">
             {console.log("Rendering...")}
             {
-              gameNumbers.map(x => <PairNumber number={x}></PairNumber>)
+              gameNumbers
+                .map((number, index) =>
+                  <PairNumber
+                    key={index}
+                    index={index}
+                    number={number}
+                    status={numberStatus(index, number)}
+                    numberClicked={manageGame}>
+                  </PairNumber>
+                )
             }
           </div>
         </div>
         <div>
-          <button onClick={shuffleNumbers} >Restart Game</button>
+          <button >Restart Game</button>
         </div>
         <div className="timer">Time Remaining: 0</div>
         <div className="game-done">
@@ -44,22 +96,23 @@ function App() {
 
 const PairNumber = (props) => {
 
-  const [number, setNumber] = useState(props.number);
-  const [isVisible, setIsVisible] = useState(false);
-
-  const toggleVisible = () => {
-
-    setIsVisible(!isVisible);
+  const styles = () => {
+    if (props.status === 'matched') {
+      return { backgroundColor: '#80ff80' };
+    } else if (props.status === 'candidate') {
+      return { backgroundColor: 'blue' };
+    }
+    return null;
   }
 
   return (
-    <button className="number" onClick={toggleVisible}>
+    <button className="number"
+      onClick={(i, n) => props.numberClicked(props.index, props.number)}
+      style={styles()} >
       {
-        isVisible ?
-          props.number :
-          "x"
+        props.status === 'matched' || props.status === 'candidate' ? props.number : "x"
       }
-    </button>
+    </button >
   )
 }
 export default App;
