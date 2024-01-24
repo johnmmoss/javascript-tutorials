c6a9de0bd0fbcd564f99762b6b0666d7fc1289ad
John Moss <johnmmoss@gmail.com>
2024-01-24 18:51:22 +0000

    Update react router example

--- ReactSamples/router-example/README.md
+++ ReactSamples/router-example/README.md
@@ -1,7 +1,1 @@
-
-Example for using react-router-dom:
-
-[React Training > React Router](https://linkhttps://reacttraining.com/react-router/web/guides/quick-start)
-
-[React-Router vs React-Router-Dom](https://stackoverflow.com/questions/42684809/react-router-vs-react-router-dom-when-to-use-one-or-the-other)
-
+React Router example from w3schools: https://www.w3schools.com/react/react_router.asp
--- ReactSamples/router-example/src/App.js
+++ ReactSamples/router-example/src/App.js
@@ -1,80 +1,21 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Blogs from "./pages/blogs";
+import Contact from "./pages/contact";
+import NoPage from "./pages/no-page";
 
 export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home() {
-  return (
-    <div class="page">
-      <h2>Home</h2>
-      <p>This is my homepage </p>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div class="page">
-      <h2>About</h2>
-      <p>This is my About</p>
-    </div>
-  );
-}
-
-function Users() {
-  return (
-    <div>
-      <PageContent header="Yeah Baby!" strapLine="Wahtcha wit hooooo!" />
-      <hr />
-      <div>Loriem ... </div>
-      <form>
-        <input type="text"></input>
-        <input type="submit" value="Submit"></input>
-      </form>
-    </div>
-  );
-}
-
-function PageContent(props) {
-  return (
-    <div class="page">
-      <h2>{props.header}</h2>
-      <p>{props.strapLine}</p>
-    </div>
-  );
-}
+      return (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      );
+    }
